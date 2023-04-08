5c0aa40be842578797acb3afaaa104cc67d1bc11
Kinnari-Shah <iampritesh13@gmail.com>
2023-04-08 16:58:13 +0530

    768px

--- src/app/components/cliants/AddToCartHeader.jsx
+++ src/app/components/cliants/AddToCartHeader.jsx
@@ -10,10 +10,10 @@ export const AddToCartHeader = ({ children }) => {
     const { totalItem, totalAmount } = useContext(CartProductsContext);
 
     return (
-        
-        <div className="flex justify-between px-10 gap-x-10 lg:gap-x-20 h-8 lg:h-16">
 
-            <div className="flex gap-x-5 lg:gap-x-20 items-center sm:justify-between flex-wrap gap-y-5 px-5 lg:px-20 py-2 bg-yellow-300 rounded-t-3xl lg:rounded-t-full w-full">
+        <div className="flex justify-between items-center px-10 gap-x-10 lg:gap-x-20 h-8 lg:h-16">
+
+            <div className="flex gap-x-5 lg:gap-x-20 items-center justify-between flex-wrap gap-y-5 px-5 lg:px-20 py-2 bg-yellow-300 rounded-t-2xl lg:rounded-t-full w-full">
 
                 <div className="font-semibold text-xl">
                     <span className="hidden lg:inline"> Shopping </span>
--- src/app/components/cliants/AddToCartHeaderCall.jsx
+++ src/app/components/cliants/AddToCartHeaderCall.jsx
@@ -9,7 +9,7 @@ const AddToCartHeaderCall = () => {
     return (
         <>
             <AddToCartHeader>
-                <Link href="/products/cart" className="bg-yellow-300 rounded-full p-5 border-red-500 border-2">
+                <Link href="/products/cart" className="bg-yellow-300 rounded-2xl lg:rounded-full p-5 border-red-500 border-2 flex justify-center items-center max-h-full">
                     <Image src='/cart-icon.svg' alt='cart' width={32} height={32} className=' svg-convert-to-black '></Image>
                 </Link>
             </AddToCartHeader>
