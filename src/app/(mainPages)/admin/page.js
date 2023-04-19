5bc5e5a1443aed04bfb706f4a8a074e7af456fa2
Kinnari-Shah <iampritesh13@gmail.com>
2023-04-19 06:35:13 +0530

    Update page.js

--- src/app/(mainPages)/admin/page.js
+++ src/app/(mainPages)/admin/page.js
@@ -1,7 +1,7 @@
 import ApiNavbar from '@/app/components/cliants/ApiNavbar';
-import ProductsCardAdmin from '@/app/componentsAdmin/ProductsCardAdmin';
 import dbConnect from '@/utils/database';
 import { PtModels2 } from '@/utils/models/allModel';
+import Image from 'next/image';
 
 import React from 'react'
 
@@ -17,7 +17,7 @@ const Admin = async () => {
     const sortAllProductsList = await PtModels2.find();
     const sortAllProductsArray = [...sortAllProductsList];
 
-  
+
 
     // const productsJSONObjectFS = await getAllProductsData();
     // const productsArrayFS = productsJSONObjectFS.products;
@@ -39,14 +39,37 @@ const Admin = async () => {
                         {sortAllProductsArray.map((curItem, index) => {
                             return (
                                 <>
-                                    <ProductsCardAdmin key={index}
-                                        id={curItem.id}
-                                        img={curItem.img}
-                                        title={curItem.title}
-                                        description={curItem.description}
-                                        price={curItem.price}
-                                    >
-                                    </ProductsCardAdmin>
+                                    <div key={index} className="w-max max-w-sm border-orange-400 border-2 rounded-lg px-10 pt-5 pb-5 h-max">
+
+                                        <h6 className="mb-2 text-2xl font-semibold text-orange-500 text-center">{`${curItem.id}`}</h6>
+
+                                        <hr className='border-t-2 border-orange-400' />
+
+                                        <div className="h-full pt-4 flex gap-x-8 items-center ">
+
+                                            <Image src={curItem.img} alt="team" width={64} height={64} className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full" />
+
+                                            <div className="flex-grow flex flex-col gap-y-4">
+
+                                                <div className="">
+
+                                                    <h2 className="text-xl text-gray-900 font-medium">
+                                                        {curItem.title}
+                                                    </h2>
+
+                                                    <p className="text-sm text-gray-500">
+                                                        {curItem.description}
+                                                    </p>
+                                                </div>
+
+
+                                                <p className="text-2xl text-black font-semibold">
+                                                    {`${curItem.price} ₹`}
+                                                </p>
+
+                                            </div>
+                                        </div>
+                                    </div>
                                 </>
                             )
                         })}
